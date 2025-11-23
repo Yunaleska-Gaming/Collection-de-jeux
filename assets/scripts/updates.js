7f662b7e5660b9224f9b8d28bc92d4d7ac247666
Yunaleska-Gaming <final-fantasy25@hotmail.fr>
2025-11-23 23:47:26 +0100

    schedule updates, achievements update

--- assets/scripts/updates.js
+++ assets/scripts/updates.js
@@ -1,66 +1,28 @@
 export const updates = [
   {
-    version: "1.3",
-    date: "18-11-2025",
+    version: "1.31",
+    date: "23-11-2025",
     messages: {
       fr: {
         features: [
-          "○ Calendrier hebdomadaire pour la semaine du 17 novembre 2025",
-          "○ Ajout d'une barre secondaire avec le profil 'Yuna'",
+          "○ Calendrier hebdomadaire pour la semaine du 24 novembre 2025",
 
-          "○ Mise à jour des succès The Evil Within (Steam)",
-          "○ Mise à jour des succès Phasmophobia (Steam)",
-          "○ Mise à jour des succès Resident Evil 6 (Steam)",
-          "○ Mise à jour des succès Megabonk (Steam)",
-          "○ Mise à jour des succès Powerwash Simulator (Steam)",
-          "○ Mise à jour des trophées The Callisto Protocol (PS5)",
-
-          "○ Ajout du jeu Powerwash Simulator 2 et de ses succès (Steam)",
-          "○ Ajout du jeu Tormented Souls 2 et de ses succès (Steam)",
-          "○ Ajout du jeu Kitaria Fables et de ses succès (Steam)",
-          "○ Ajout du jeu Stray et de ses succès (Steam)",
-          "○ Ajout du jeu Biped 2 et de ses succès (Steam)",
-          "○ Ajout du jeu Bongo Cat et de ses succès (Steam)",
-          "○ Ajout du jeu Beacon Pines et de ses succès (Steam)",
-          "○ Ajout du jeu Elypse et de ses succès (Steam)",
-          "○ Ajout du jeu Alan Wake II et de ses trophées (PS5)",
-          "○ Ajout du jeu Tormented Souls II et de ses trophées (PS5)"
+          "○ Mise à jour des succès The Room (100%) (Steam)",
+          "○ Mise à jour des succès Kitaria Fables (Steam)",
+          "○ Mise à jour des succès Powerwash Simulator (Steam)"
         ],
         fixes: [
-          "○ Correction de la couleur du patch notes sur la page Steam",
-          "○ La version sur le footer se met désormais automatiquement à jour",
-          "○ Réparation de la couleur indiquant que l'on est sur la page RetroAchievements",
-          "○ Version mobile maintenant adaptée correctement",
         ]
       },
       en: {
         features: [
-          "○ Weekly schedule for the week of the 17th of november 2025",
-          "○ Added a secondary bar with a Yuna's Profile button",
+          "○ Weekly schedule for the week of the 24th of november 2025",
 
-          "○ Updated The Evil Within achievements (Steam)",
-          "○ Updated Phasmophobia achievements (Steam)",
-          "○ Updated Resident Evil 6 achievements (Steam)",
-          "○ Updated Megabonk achievements (Steam)",
-          "○ Updated Powerwash Simulator achievements (Steam)",
-          "○ Updated The Callisto Protocol trophies (PS5)",
-
-          "○ Added the game Powerwash Simulator 2 and its achievements (Steam)",
-          "○ Added the game Tormented Souls 2 and its achievements (Steam)",
-          "○ Added the game Kitaria Fables and its achievements (Steam)",
-          "○ Added the game Stray and its achievements (Steam)",
-          "○ Added the game Biped 2 and its achievements (Steam)",
-          "○ Added the game Bongo Cat and its achievements (Steam)",
-          "○ Added the game Beacon Pines and its achievements (Steam)" ,
-          "○ Added the game Elypse and its achievements (Steam)",
-          "○ Added the game Alan Wake II and its trophies (PS5)",
-          "○ Added the game Tormented Souls II and its trophies (PS5)"
+          "○ Updated The Room achievements (100%) (Steam)",
+          "○ Updated Kitaria Fables achievements (Steam)",
+          "○ Updated Powerwash Simulator achievements (Steam)"
         ],
         fixes: [
-          "○ Fixed patch notes colors on the Steam page",
-          "○ Number of the website version is now updating itself in the footer",
-          "○ Fixed the color of the arrow, showing where we are on the RetroAchievements page",
-          "○ Mobile version now adapted properly",
         ]
       },
       pt: {
