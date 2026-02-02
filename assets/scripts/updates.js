3ebfcb34688fee328024d2c6437cce3397c6e18e
Yunaleska-Gaming <final-fantasy25@hotmail.fr>
2026-02-02 10:26:28 +0100

    updated weekly schedule, updated trophies and achievements

--- assets/scripts/updates.js
+++ assets/scripts/updates.js
@@ -1,17 +1,18 @@
 export const updates = [
   {
-    version: "1.41",
-    date: "19-01-2026",
+    version: "1.42",
+    date: "02-02-2026",
     messages: {
       fr: {
         features: [
-          "○ Planning de la semaine du 19 janvier 2026",
+          "○ Planning de la semaine du 02 février 2026",
 
-          "○ Mise à jour des succès Temtem Swarm (Steam)",
           "○ Mise à jour des succès Tomb Raider I•II•III Remastered (Steam)",
           "○ Mise à jour des succès Shape of Dreams (Steam)",
           "○ Mise à jour des succès Nexomon (Steam)",
-          "○ Mise à jour des succès Metaphor: ReFantazio (Steam)"
+          "○ Mise à jour des succès Megabonk (Steam)",
+          "○ Mise à jour des trophées Tormented Souls [Platine] (PS4)",
+          "○ Mise à jour des trophées Tormented Souls (PS5)"
         ],
         fixes: [
           
@@ -19,17 +20,14 @@ export const updates = [
       },
       en: {
         features: [
-          "○ Weekly Schedule from the 12th january",
+          "○ Weekly Schedule from the 2nd of February 2026",
 
-          "○ Updated Dead Island 2 achievements (Steam)",
-          "○ Updated The Spell Brigade achievements (Steam)",
-          "○ Updated Sworn achievements (100%) (Steam)",
-          "○ Updated Final Fantasy VII Remake Intergrade achievements (Steam)",
-          "○ Updated Temtem Swarm achievements (Steam)",
           "○ Updated Tomb Raider I•II•III Remastered achievements (Steam)",
-          
-          "○ Added Shape of Dreams and its achievements (Steam)",
-          "○ Added Nexomon and its achievements (Steam)",
+          "○ Updated Shape of Dreams achievements (Steam)",
+          "○ Updated Nexomon (Steam)",
+          "○ Updated Megabonk (Steam)",
+          "○ Updated Tormented Souls trophies [platinum] (PS4)",
+          "○ Updated Tormented Souls trophies (PS5)"
         ],
         fixes: [
           "○ Fixed the title of Tomb Raider IV•V•VI (I - III previously)",
