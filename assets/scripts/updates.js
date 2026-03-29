202ba9706711d6e3c87d744724f0cd3e4b771665
Yunaleska-Gaming <final-fantasy25@hotmail.fr>
2026-03-29 22:54:22 +0200

    updated PE2 achievements

--- assets/scripts/updates.js
+++ assets/scripts/updates.js
@@ -13,6 +13,7 @@ export const updates = [
           "○ Mise à jour des succès Savara (Steam)",
           "○ Mise à jour des succès Tormented Souls II (Steam)",
           "○ Mise à jour des succès Final Fantasy XII: The Zodiac Age (Steam)",
+          "○ Mise à jour des succès Parasite Eve II (PS1)",
 
           "○ Ajout de Fatal Frame II: Crimson Butterfly Remake et de ses succès (Steam)",
           "○ Ajout de Godbreakers et de ses succès (Steam)"
@@ -31,6 +32,7 @@ export const updates = [
           "○ Updated Savara achievements (Steam)",
           "○ Updated Tormented Souls II (Steam)",
           "○ Updated Final Fantasy XII: The Zodiac Age achievements (Steam)",
+          "○ Updated Parasite Eve II achievements (PS1)",
 
           "○ Added Fatal Frame II: Crimson Butterfly Remake and its achievements (Steam)",
           "○ Added Godbreakers and its achievements (Steam)"
