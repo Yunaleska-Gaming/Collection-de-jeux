2f0d462eb6aae1bcf7857606c6719ba3736bc246
Yunaleska-Gaming <final-fantasy25@hotmail.fr>
2026-05-17 21:30:56 +0200

    weekly update

--- assets/scripts/updates.js
+++ assets/scripts/updates.js
@@ -1,18 +1,16 @@
 export const updates = [
   {
-    version: "1.53",
-    date: "03-05-2026",
+    version: "1.54",
+    date: "17-05-2026",
     messages: {
       fr: {
         features: [
-          "○ Planning de la semaine du 04 mai 2026",
+          "○ Planning de la semaine du 18 mai 2026",
         
-          "○ Mise à jour des succès Nexomon : Extinction (Steam)",
-          "○ Mise à jour des succès The Evil Within (Steam)",
           "○ Mise à jour des succès The Spell Brigade (Steam)",
           "○ Mise à jour des succès Tomb Raider IV•V•VI Remastered (Steam)",
-          "○ Mise à jour des succès Resident Evil 2 (PS1)",
-          "○ Ajout du lien vers la playlist pour le challenge Final Fantasy VIII"
+
+          "○ Ajout du jeu Neverness to Everness et de ses trophées (PS5)"
         ],
         fixes: [
           
@@ -25,14 +23,11 @@ export const updates = [
       },
       en: {
         features: [
-          "○ Weekly Schedule from the 4th of May 2026",
+          "○ Weekly Schedule from the 18th of May 2026",
 
-          "○ Updated Nexomon : Extinction achievements (Steam)",
-          "○ Updated The Evil Within achievements (Steam)",
           "○ Updated The Spell Brigade achievements (Steam)",
           "○ Updated Tomb Raider IV•V•VI Remastered achievements (Steam)",
-          "○ Updated Resident Evil 2 achievements (PS1)",
-          "○ Added a link to the playlist of the Final Fantasy VIII Challenge"
+          "○ Added the game Neverness to Everness and its trophies (PS5)"
 
         ],
         fixes: [
@@ -45,13 +40,10 @@ export const updates = [
       },
       pt: {
         features: [
-           "○ Agenda da semana de 04 de maio de 2026",
+           "○ Agenda da semana de 18 de maio de 2026",
 
-           "○ Atualização das conquistas de Nexomon : Extinction (Steam)",
-           "○ Atualização das conquistas de The Evil Within (Steam)",
            "○ Atualização das conquistas de The Spell Brigade (Steam)",
            "○ Atualização das conquistas de Tomb Raider IV•V•VI Remastered (Steam)",
-           "○ Atualização das conquistas de Resident Evil 2 (PS1)"
         ],
         fixes: [
           
