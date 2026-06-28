0534c1d81e5360d21c2098026523daed8c0e33c0
Yunaleska-Gaming <final-fantasy25@hotmail.fr>
2026-06-28 16:50:03 +0200

    weekly update 28th june 2026, ver.1.56

--- assets/scripts/updates.js
+++ assets/scripts/updates.js
@@ -1,30 +1,16 @@
 export const updates = [
   {
-    version: "1.55",
-    date: "14-06-2026",
+    version: "1.56",
+    date: "28-06-2026",
     messages: {
       fr: {
         features: [
-          "○ Planning de la semaine du 15 juin 2026",
+          "○ Planning de la semaine du 28 juin 2026",
         
-          "○ Mise à jour des succès The Room 3 [100%] (Steam)",
-          "○ Mise à jour des succès The Room 4 [100%] (Steam)",
           "○ Mise à jour des succès Tomb Raider IV•V•VI Remastered (Steam)",
-          "○ Mise à jour des succès The Evil Within (Steam)",
-          "○ Mise à jour des succès Shape of Dreams [100%] (Steam)",
-          "○ Mise à jour des succès Kitaria Fables [100%] (Steam)",
           "○ Mise à jour des succès Goblin Cleanup (Steam)",
-          "○ Mise à jour des succès Content Warning (Steam)",
 
-          "○ Ajout du jeu Ground Zero et de ses succès (Steam)",
-          "○ Ajout du jeu Star Fire: Eternal Cycle et de ses succès (Steam)",
-          "○ Ajout du jeu House Flipper 2 et de ses succès (Steam)",
-          "○ Ajout du jeu Endling: Extinction is Forever et de ses succès (Steam)",
-          "○ Ajout du jeu Spirit of the North et de ses succès (Steam)",
-          "○ Ajout du jeu Disney Epic Mickey: Rebrushed et de ses succès (Steam)",
-          "○ Ajout du jeu Pragmata et de ses succès (Steam)",
-          "○ Ajout du jeu Octopath Traveler II et de ses succès (Steam)",
-          "○ Ajout du jeu Star Wars: Survivor et de ses succès (PS5)"
+          "○ Ajout du jeu Mages of Mystralia et de ses succès (Steam)",
         ],
         fixes: [
           
@@ -37,26 +23,12 @@ export const updates = [
       },
       en: {
         features: [
-          "○ Weekly Schedule from the 15th of June 2026",
+          "○ Weekly Schedule from the 28th of June 2026",
 
-          "○ Updated The Room 3 achievements [100%] (Steam)",
-          "○ Updated The Room 4 achievements [100%] (Steam)",
           "○ Updated Tomb Raider IV•V•VI Remastered achievements (Steam)",
-          "○ Updated The Evil Within achievements(Steam)",
-          "○ Updated Shape of Dreams achievements [100%] (Steam)",
-          "○ Updated Kitaria Fables achievements [100%] (Steam)",
           "○ Updated Goblin Cleanup achievements (Steam)",
-          "○ Updated Content Warning achievements (Steam)",
 
-          "○ Added the game Ground Zero and its achievements (Steam)",
-          "○ Added the game Star Fire: Eternal Cycle and its achievements (Steam)",
-          "○ Added the game House Flipper 2 and its achievements (Steam)",
-          "○ Added the game Endling: Extinction is Forever and its achievements (Steam)",
-          "○ Added the game Spirit of the North and its achievements (Steam)",
-          "○ Added the game Disney Epic Mickey: Rebrushed and its achievements (Steam)",
-          "○ Added the game Pragmata and its achievements (Steam)",
-          "○ Added the game Octopath Traveler II and its achievements (Steam)",
-          "○ Added the game Star Wars: Survivor and its trophies (PS5)"
+          "○ Added the game Mages of Mystralia and its achievements (Steam)",
 
         ],
         fixes: [
